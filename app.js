0ff8e859c244e6c6bf84d555f7f644ed72a90087
Sahil Mahendrakar <sahilm020@gmail.com>
2020-02-09 08:47:27 -0500

    added Mongoose, added student and startup schema and models

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
 var express = require('express')
 var app = express()
+app.use(express.static("public"));
+
+var mongoose = require('mongoose')
+mongoose.connect("mongodb://localhost/internet-db")
+
+var studentSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    name: String,
+    image: String,
+    email: String,
+    grade: Number,
+    skills: Array,
+    resume: String,
+    location: String,
+})
+
+var startupSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    name: String,
+    description: String,
+    jobs: Array,
+    location: String,
+})
+
+var Student = new mongoose.model("Student", studentSchema)
+var Startup = new mongoose.model("Startup", startupSchema)
+
 
 app.get("/", function(req, res){
-    res.send("Hi!")
+    res.render("index.html")
 })
+
 
 app.listen(8080, function(){
     console.log("Listening on Port 8080")
